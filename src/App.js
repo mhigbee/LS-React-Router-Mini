61dd5b7c90678cccb8c7728c914389e77688d04e
Matt Higbee <mhigbee@hotmail.com>
2017-08-10 19:49:23 -0600

    finishing mini sprint

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
 import { Home, About, Contact} from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
-    <Navigation /> 
+    <Navigation />
+    <Route exact path="/" component={Home}/>
+    <Route path="/contact" component={Contact}/>
+    <Route path="/about" component={About}/>
   </div>
 )
 
+
+
 export default App;
+
